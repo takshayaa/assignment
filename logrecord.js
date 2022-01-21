9d7e379548a37b03443454df61d99cd681ca7788
takshayaa <94888393+takshayaa@users.noreply.github.com>
2022-01-21 14:49:25 +0000

    Add files via upload

---
+++ logrecord.js
@@ -0,0 +1,34 @@
+var readline=require('readline-sync');
+fs = require('fs')
+function divide(x,y)
+{
+    if(y==0)
+        throw new Error('can not divide by Zero');
+    else   
+        console.log("Division is "+x/y);
+}
+//handle the code which throws exception
+function add(x,y){
+    console.log("Addition is ",x+y);
+}
+function sub(x,y){
+    console.log("Substraction is",x-y);
+}
+function mul(x,y){
+    console.log("Multiplication is",x*y);
+}
+
+
+
+var a=parseInt(readline.question("Enter first number:"));
+var b=parseInt(readline.question("Enter second number:"));
+add(a,b);
+sub(a,b);
+mul(a,b);
+try {
+    divide(a,b); 
+} catch (err) {
+    fs.writeFileSync('error.txt',err.toString());
+     console.log("Error ocerd:"+err);
+
+}
